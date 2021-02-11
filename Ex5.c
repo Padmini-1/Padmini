68ea8e3fed889eb81b2c707521aec6fedb4e7ecf
Padmini-1 <77256460+Padmini-1@users.noreply.github.com>
2021-02-11 15:51:29 +0530

    Add files via upload

---
+++ Ex5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int n=5;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=n;j>=1;j--)
+        {
+            if(j>i)
+            printf(" ");
+            else
+                printf("%d",j);
+        }
+        printf("\n");
+    }
+}
